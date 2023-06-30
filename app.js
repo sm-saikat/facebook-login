94138755509a5e4e7b416ab679193bdeeadd8ad5
smsaikat <mail.smsaikat@gmail.com>
2023-06-30 23:03:10 +0600

    all done

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser')
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.text())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log('Server is running')
+})
+
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+
+app.post('/login', (req, res)=>{
+    console.log(req.body)
+})
